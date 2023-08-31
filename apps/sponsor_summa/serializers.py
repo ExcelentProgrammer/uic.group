ff03d6e7c626e3a278021a9ae8f0999a869fb819
Samandar <azamov.samandar.programmer@gmail.com>
2023-08-31 23:40:47 +0500

    update serializer

--- apps/sponsor_summa/serializers.py
+++ apps/sponsor_summa/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 
+from apps.sponsor.serializers import SponsorDetailSerializer
 from apps.sponsor_summa.models import SponsorSumma
+from apps.student.serializers import StudentSerializer
 
 
 class SponsorSummaSerializer(serializers.ModelSerializer):
+    sponsor = SponsorDetailSerializer()
+    student = StudentSerializer()
+
     class Meta:
         model = SponsorSumma
         fields = [
